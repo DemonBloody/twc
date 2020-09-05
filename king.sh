8c72708232f3a444b631b9afcf98ebf45e85df46
DemonBloody <70643082+DemonBloody@users.noreply.github.com>
2020-09-04 23:07:47 -0300

    Create king.sh

---
+++ king.sh
@@ -0,0 +1,108 @@
+_king () {
+# /enterFight
+	HPER='52'
+	RPER='1'
+	ITVL='2.94'
+	echo -e "\nKing"
+	echo $URL
+	SRC=$(w3m -debug -dump_source $ENC $URL/king/enterGame -o user_agent="$(shuf -n1 .ua)")
+	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/king/' | head -n1 | awk -F\' '{ print $2 }')
+	echo -e " 👣 Entering...\n$ACCESS"
+# /wait
+	echo " 😴 Waiting..."
+        EXIT=$(echo $SRC | grep -o 'king/kingatk/')
+	while [[ -z $EXIT ]] ; do
+		[[ $(date +%M) = 30 && $(date +%S) > 19 ]] && break
+		echo -e " 💤	...\n$ACCESS"
+		SRC=$(w3m -dump_source $ENC "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/king/' | head -n1 | awk -F\' '{ print $2 }')
+		EXIT=$(echo $SRC | grep -o 'king/kingatk/')
+	done
+# /game
+	FULL=$(echo $SRC | sed "s/alt/\\n/g" | grep "hp" | head -n1 | awk -F\< '{ print $2 }' | awk -F\> '{ print $2 }' | tr -cd "[[:digit:]]")
+	_access
+	HP3=$HP1
+	ddg=4
+	hl=18
+	grss=12
+	until [[ -n $BEXIT && -z $OUTGATE ]] ; do
+		[[ $(date +%M) = 4[01] ]] && break
+# /kingatk
+		if [[ -n $KINGATK && $ddg -ne 4 && $hl -ne 18 && $HP2 -gt 15 ]] ; then
+			echo '👑'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$KINGATK" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+# /dodge
+		elif [[ $ddg -ge 4 && $hl -ne 18 && $HP3 -ne $HP1 ]] ; then
+			echo '🛡️'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$DODGE" -o user_agent="$(shuf -n1 .ua)")
+			ddg=0
+			HP3=$HP1
+			_access
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+# /heal
+		elif [[ $hl -ge 18 && $HP1 -le $HLHP ]] ; then
+			RPER='7'
+			echo "🆘 HP < $HPER%"
+			SRC=$(w3m -debug -dump_source $ENC "$URL$HEAL" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			hl=0
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+# /grass
+		elif [[ $HP2 -le 15 || -z $KINGATK && $ddg != [34] && $hl != 1[78] && $grss -ge 12 ]] ; then
+			HPER='30'
+			RPER='13'
+			echo '🙌'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$GRASS" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			grss=0
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+# /stone
+#		[[ `expr $HP1 + $HP1 \* 1 \/ 100` -le $HP2 ]]
+			echo '💪'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$STONE" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+# /random
+		elif [[ `expr $HP1 + $HP1 \* $RPER \/ 100` -le $HP2 && $ddg -ne 4 && $hl -ne 18 && $grss -ne 12 ]] ; then
+			echo '🔁'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$ATTACKRANDOM" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+
+# /atk
+		else
+			echo '🎯'
+			SRC=$(w3m -debug -dump_source $ENC "$URL$ATTACK" -o user_agent="$(shuf -n1 .ua)")
+			_access
+			sleep $ITVL
+			ddg=$[$ddg+1]
+			hl=$[$hl+1]
+			grss=$[$grss+1]
+		fi
+	done
+# /view
+	echo ""
+	w3m -debug $ENC $URL/king -o user_agent="$(shuf -n1 .ua)" | head -n15 | tail -n14 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d" | grep --color $ACC
+	echo "King (✔)"
+	sleep 30
+}
